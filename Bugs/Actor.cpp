ef9e8dd5b258d487f419e54bf456f7daf6c49aff
clarkminor@g.ucla.edu <clarkminor@g.ucla.edu>
2017-03-01 13:07:13 -0800

    I'm probably going to hemorrhage this branch altogether but maybe I can use some of it's functionality for my main

--- Bugs/Actor.cpp
+++ Bugs/Actor.cpp
@@ -1,4 +1,100 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-// Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+
+//====================================Actor class
+
+//GraphObject::GraphObject(int imageID, int startX, int startY, Direction dir = right, int depth = 0, double size = 0.25)
+
+Actor::Actor(int imageID, int startX, int startY, Direction startDirection, int depth)
+:GraphObject(imageID, startX, startY, startDirection, depth)
+{
+    
+}
+
+
+//====================================EnergyHolder class
+
+EnergyHolder::EnergyHolder(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy):
+Actor(imageID, startX, startY, startDirection, depth)
+{
+    
+}
+
+bool EnergyHolder::blocksMovement(Actor* inQuestion) const
+{
+    return false;
+}
+
+void EnergyHolder::changeEnergy(int amt)
+{
+    m_energy+=amt;
+}
+
+
+//====================================Insect class
+
+Insect::Insect(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy):
+EnergyHolder(imageID, startX, startY, startDirection, depth, startingEnergy)
+{
+    
+}
+
+int Insect::getRestCount()const
+{
+    return m_restCount;
+}
+
+void Insect::increaseRestCount(int amt)
+{
+    m_restCount+= amt;
+}
+
+int Insect::getDistance() const
+{
+    return m_curDist;
+}
+
+void Insect::setDistance(int distance)
+{
+    m_curDist= distance;
+}
+
+//====================================GrassHopper class
+
+GrassHopper::GrassHopper(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy):
+Insect(imageID, startX, startY, startDirection, depth, startingEnergy)
+{
+    
+}
+
+
+//====================================BabyGrassHopper class
+
+BabyGrassHopper::BabyGrassHopper(int startX, int startY):
+GrassHopper(IID_BABY_GRASSHOPPER, startX, startY, none, BABYHOPPER_START_DEPTH, BABYHOPPER_START_HP)
+{
+    setDirection(getRandomDir());
+    setDistance(randInt(2, 12)); //walking instructions
+    
+    //m_restCount==0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Bugs/Actor.h
+++ Bugs/Actor.h
@@ -3,6 +3,106 @@
 
 #include "GraphObject.h"
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+const int BABYHOPPER_START_HP= 500;
+const int BABYHOPPER_START_DEPTH= 1;
+const int GRASSHOPPER_REST_PERIOD= 2;
+
+//====================================Actor class (abstract)
+class Actor: public GraphObject
+{
+public:
+    Actor(int imageID, int startX, int startY, Direction startDirection, int depth);
+    virtual void doSomething()=0;
+    virtual bool blocksMovement(Actor* inQuestion) const=0;
+    
+};
+
+//====================================EnergyHolder class (abstract)
+class EnergyHolder: public Actor
+{
+public:
+    EnergyHolder(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy);
+    virtual void changeEnergy(int amt);
+    virtual bool blocksMovement(Actor* inQuestion) const; //returns false
+    
+private:
+    int m_energy;
+};
+
+//====================================Insect class (abstract)
+class Insect: public EnergyHolder
+{
+public:
+    Insect(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy);
+    int getRestCount() const;
+    void increaseRestCount(int amt);
+    int getDistance() const;
+    void setDistance(int distance);
+    
+    
+    
+private:
+    int m_restCount=0;
+    int m_curDist; //current walking distance
+    
+};
+
+//====================================GrassHopper class (abstract)
+class GrassHopper: public Insect
+{
+public:
+    GrassHopper(int imageID, int startX, int startY, Direction startDirection, int depth, int startingEnergy);
+    virtual void doSomething();
+
+    
+private:
+    
+};
+
+
+//====================================BabyGrassHopper class
+class BabyGrassHopper: public GrassHopper
+{
+public:
+    BabyGrassHopper(int startX, int startY);
+    virtual void doSomething();
+    void move();
+    
+private:
+    
+    
+    
+};
+
+
+//====================================Auxilliary functions
+
+//randInt(): Return a uniformly distributed random int from min to max, inclusive
+GraphObject::Direction getRandomDir() //return a random direction: up, right, down, or left
+{
+    int d= randInt(1, 4);
+    
+    switch (d)
+    {
+        case 1:
+            return GraphObject::Direction::up;
+            break;
+        case 2:
+            return GraphObject::Direction::right;
+            break;
+        case 3:
+            return GraphObject::Direction::down;
+            break;
+        case 4:
+            return GraphObject::Direction::left;
+            break;
+        default:
+            std::cerr << "This is bad\n";
+            exit(1);
+            break;
+    }
+}
+
+
 
 #endif // ACTOR_H_
